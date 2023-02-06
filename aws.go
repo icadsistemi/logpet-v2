9d8327b63e01f33f98a0bcaf7fdbad56de680fdf
Emanuele Rocco Petrone <github@emaele.me>
2023-02-06 15:41:28 +0100

    delete bearer token from headers before logging

--- aws.go
+++ aws.go
@@ -15,6 +15,9 @@ func (l *StandardLogger) LogAPIGatewayProxyRequest(event events.APIGatewayProxyR
 
 	email, _ := ReadEmailFromAPIGatewayProxyRequestEvent(event)
 
+	// Remove Bearer token from headers before logging
+	delete(event.Headers, "Authorization")
+
 	l.CustomFields["http.query_parameters"] = event.QueryStringParameters
 	l.CustomFields["http.path_parameters"] = event.PathParameters
 	l.CustomFields["http.headers"] = event.Headers
