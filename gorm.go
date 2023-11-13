99dd800595f8260f8366e62ebdc585ca75cd6ae4
Emanuele Rocco Petrone <github@emaele.me>
2023-11-13 12:09:59 +0100

    implement message

--- gorm.go
+++ gorm.go
@@ -19,8 +19,8 @@ func (g GormLogger) LogMode(logger.LogLevel) logger.Interface {
 	return g
 }
 
-func (g GormLogger) Info(context.Context, string, ...interface{}) {
-
+func (g GormLogger) Info(ctx context.Context, msg string, data ...interface{}) {
+	g.Logger.SendDebugfLog("%s", nil, msg)
 }
 func (g GormLogger) Warn(context.Context, string, ...interface{}) {
 
