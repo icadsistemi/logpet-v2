853895ecce2ba54284425c7c54e2aed080b1ba10
Emanuele Rocco Petrone <github@emaele.me>
2023-11-13 11:48:50 +0100

    logpet v2

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/icadsistemi/logpet
+module github.com/icadsistemi/logpet-v2
 
 go 1.13
 
--- gorm.go
+++ gorm.go
@@ -1,8 +1,7 @@
 package logpet
 
 import (
-	"fmt"
-	"strconv"
+	"context"
 	"time"
 )
 
@@ -14,46 +13,14 @@ type GormLogger struct {
 	Logger *StandardLogger
 }
 
-// Print handles log events from Gorm for the custom logger.
-func (gLogger *GormLogger) Print(v ...interface{}) {
-	fields := map[string]interface{}{
-		"section": "database",
-	}
+func (g *GormLogger) Info(context.Context, string, ...interface{}) {
 
-	if nil == v {
-		gLogger.Logger.SendDebugLog("v database arguments are empty", fields)
-		return
-	}
-
-	switch v[0] {
-	case "sql":
-
-		if len(v) != 6 {
-			gLogger.Logger.SendErrfLog("wrong db log format, %v", fields, v)
-			return
-		}
-
-		queryfields := map[string]string{
-			"execution_time": v[2].(time.Duration).String(),
-			"arguments":      fmt.Sprintf("%v", v[4]),
-			"rows_affected":  strconv.FormatInt(v[5].(int64), 10),
-			"function_line":  v[1].(string),
-		}
-
-		fields["query"] = queryfields
-
-		gLogger.Logger.SendDebugfLog(v[3].(string), fields)
-	case "log":
-		if len(v) != 3 {
-			gLogger.Logger.SendErrfLog("wrong db log format, %v", fields, v)
-			return
-		}
-
-		queryfields := map[string]string{
-			"function_line": v[1].(string),
-		}
-
-		fields["query"] = queryfields
-		gLogger.Logger.SendWarnfLog("%v", fields, v[2])
-	}
 }
+func (g *GormLogger) Warn(context.Context, string, ...interface{}) {
+
+}
+func (g *GormLogger) Error(context.Context, string, ...interface{}) {
+
+}
+
+func (g *GormLogger) Trace(ctx context.Context, begin time.Time, fc func() (sql string, rowsAffected int64), err error)
