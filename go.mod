8810a0eb1e4a7f092df14b9554b0a5e3cf8d305d
Emanuele Rocco Petrone <github@emaele.me>
2023-11-13 11:56:56 +0100

    test

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/aws/aws-lambda-go v1.37.0
 	github.com/gin-gonic/gin v1.9.1
 	github.com/sirupsen/logrus v1.6.0
+	gorm.io/gorm v1.25.5
 )
--- gorm.go
+++ gorm.go
@@ -3,6 +3,8 @@ package logpet
 import (
 	"context"
 	"time"
+
+	"gorm.io/gorm/logger"
 )
 
 func CreateGormLogger(logger *StandardLogger) *GormLogger {
@@ -13,14 +15,16 @@ type GormLogger struct {
 	Logger *StandardLogger
 }
 
-func (g *GormLogger) Info(context.Context, string, ...interface{}) {
+func (g GormLogger) LogMode(logger.LogLevel) {}
+
+func (g GormLogger) Info(context.Context, string, ...interface{}) {
 
 }
-func (g *GormLogger) Warn(context.Context, string, ...interface{}) {
+func (g GormLogger) Warn(context.Context, string, ...interface{}) {
 
 }
-func (g *GormLogger) Error(context.Context, string, ...interface{}) {
+func (g GormLogger) Error(context.Context, string, ...interface{}) {
 
 }
 
-func (g *GormLogger) Trace(ctx context.Context, begin time.Time, fc func() (sql string, rowsAffected int64), err error)
+func (g GormLogger) Trace(ctx context.Context, begin time.Time, fc func() (sql string, rowsAffected int64), err error)
